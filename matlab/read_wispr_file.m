081c3308af10f6a5bdcc2a837c5416aeed6cbf6c
selenefregosi-noaa <selene.fregosi@noaa.gov>
2024-12-09 11:58:04 -0800

    output hdr as a string, pull WISPR ver
    for printing into a fileheaders.txt

--- matlab/read_wispr_file.m
+++ matlab/read_wispr_file.m
@@ -1,9 +1,9 @@
-function [hdr, data, time, stamp] = read_wispr_file(name, first, last)
+function [hdr, data, time, stamp, hdrStrs] = read_wispr_file(name, first, last)
 %
 % matlab script to plot wispr data
 %
 % A data file consists of an ascii file header followed by binary data
-% buffers. The ascii header is formatted as matlab expressions. 
+% buffers. The ascii header is formatted as matlab expressions.
 % The binary data words are formatted as signed 16 or 24 bit integers.
 %
 % The data file format is:
@@ -14,14 +14,14 @@ function [hdr, data, time, stamp] = read_wispr_file(name, first, last)
 % - adc buffer N
 % where N is the number of adc buffers per file
 %
-% The number of adc buffers is defined as 
+% The number of adc buffers is defined as
 % number_buffers = file_size*512 / buffer_size;
 %
-% The total data file size is always a multiple of 512 bytes blocks. 
+% The total data file size is always a multiple of 512 bytes blocks.
 % The variable 'file_size' is the number of 512 blocks in the file.
 %
 % Each adc buffer is of length 'buffer_size' bytes.
-% The adc buffer is always a multiple of 512 bytes blocks (32 blocks in most cases). 
+% The adc buffer is always a multiple of 512 bytes blocks (32 blocks in most cases).
 % Each adc buffer contains a fixed number of sample (samples_per_buffer).
 % Each sample is of fixed size in bytes (sample_size).
 % The sample size can be 2 or 3.
@@ -34,11 +34,12 @@ function [hdr, data, time, stamp] = read_wispr_file(name, first, last)
 fp = fopen( name, 'r', 'ieee-le' );
 
 % back compatability
+wisprVersion = '';	 %#ok<NASGU>  might get overridden below
 instrument_id = [];
 location_id = [];
 sensor_id = [];
 platform_id = [];
-timestamp = 0; 
+timestamp = 0;
 time = [];
 second = 0;
 usec = 0;
@@ -46,8 +47,21 @@ adc_type = 'LTC2512';
 adc_df = [];
 decimation = [];
 channels = 1;
+version = [];
 
-% read and eval the ascii header lines
+% read and eval the ascii header lines - put into a str and cell array
+% hdrCell = {};
+% first line with wispr ver is a comment so doesn't get evaluated but still
+% want to parse that info
+ln1 = fgets(fp);
+[wisprVersion,nScan] = sscanf(ln1, '%% WISPR %s');		%#ok<ASGLU>
+hdrStrs = {strip(ln1)};
+if (nScan ~= 1)
+    fprintf(1, 'Something weird with WISPR ver read. Exiting\n');
+    return
+end
+
+% use eval on the remaining lines
 for n = 1:18
     %str = fgets(fp, 64); % read 64 chars max in each line
     str = fgets(fp); % read full line
@@ -57,6 +71,7 @@ for n = 1:18
     end
     %fprintf('%s', str);
     eval(str);
+    hdrStrs = [hdrStrs; {strip(str)}];
 end
 
 if(sample_size == 2)
@@ -79,8 +94,9 @@ padding_per_buffer = buffer_size - timestamp - (samples_per_buffer * sample_size
 % buffer duration in secs
 dt = 1.0 / sampling_rate;
 duration = samples_per_buffer * dt;
- 
+
 % fill the header structure
+hdr.wisprVersion = wisprVersion;
 hdr.time = time; % back compatability
 hdr.second = second;
 hdr.usec = usec;
@@ -88,6 +104,7 @@ hdr.sensor_id = sensor_id;
 hdr.platform_id = platform_id;
 hdr.instrument_id = instrument_id;
 hdr.location_id = location_id;
+hdr.version = version;
 hdr.file_size = file_size;
 hdr.buffer_size = buffer_size;
 hdr.samples_per_buffer = samples_per_buffer;
@@ -148,16 +165,16 @@ for n = 1:num_bufs
 
     t0 = t0 + duration;
 
-    % read buffer timestamp, which is time the buffer finished, 
+    % read buffer timestamp, which is time the buffer finished,
     % so remove the buffer duration
-    if( timestamp == 8 )   
-        s = fread(fp, 1, 'uint32' ); 
+    if( timestamp == 8 )
+        s = fread(fp, 1, 'uint32' );
         us = fread(fp, 1, 'uint32' );
-        stamp(n) = (s + 0.000001 * us) - duration; 
+        stamp(n) = (s + 0.000001 * us) - duration;
     elseif( timestamp == 6 )
-        s = fread(fp, 1, 'uint16' ); 
+        s = fread(fp, 1, 'uint16' );
         us = fread(fp, 1, 'uint32' );
-        stamp(n) = ((second + s) + 0.000001 * us) - duration; 
+        stamp(n) = ((second + s) + 0.000001 * us) - duration;
         %fprintf('timestamp %d bytes, padding %d bytes, ', timestamp, padding_per_buffer);
         %fprintf('sec = %lu, usec = %d\n', sec, usec);
     else
@@ -171,8 +188,8 @@ for n = 1:num_bufs
     data(:,n) = double(raw)*q;
 
     time(:,n) = stamp(n) + dt*(0:(samples_per_buffer-1));
-%    time(:,n) = t0 + dt*(0:(samples_per_buffer-1))';
-    
+    %    time(:,n) = t0 + dt*(0:(samples_per_buffer-1))';
+
 end
 
 fclose(fp);
